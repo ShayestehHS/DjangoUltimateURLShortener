92c1ecc9c48c3a302fd64ac4243c24128feeb8db
shayestehhs <shayestehhs1@gmail.com>
2024-06-17 21:20:49 +0330

    Add test for expired and invalid token

--- urls/tests/test_views.py
+++ urls/tests/test_views.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
+
 from django.conf import settings
 from django.test import TestCase
+from django.utils.timezone import now
 from rest_framework import status
 from rest_framework.reverse import reverse
 
@@ -24,5 +27,21 @@ class TestRedirectUrlView(TestCase):
     def test_redirect_with_invalid_token(self):
         with self.assertNumQueries(1):
             response = self.client.get(get_redirect_url("abcdf"))
+            self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+            self.assertEqual(response["location"], settings.URL_SHORTENER_404_PAGE)
+
+    def test_redirect_with_expired_token(self):
+        url = Url.objects.create(url="https://example.com", expiration_date=now() - timedelta(days=1))
+
+        with self.assertNumQueries(1):
+            response = self.client.get(get_redirect_url(url.token))
+            self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+            self.assertEqual(response["location"], settings.URL_SHORTENER_404_PAGE)
+
+    def test_redirect_with_ready_to_set_token(self):
+        url = Url.objects.create_ready_to_set_token()
+
+        with self.assertNumQueries(1):
+            response = self.client.get(get_redirect_url(url.token))
             self.assertEqual(response.status_code, status.HTTP_302_FOUND)
             self.assertEqual(response["location"], settings.URL_SHORTENER_404_PAGE)
