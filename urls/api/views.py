f6f2a2cd4e378d1ad97a29544cd4ff3781733bdc
shayestehhs <shayestehhs1@gmail.com>
2024-06-17 21:15:23 +0330

    Update redirect end-point to redirect to 404 page if token is invalid

--- urls/api/views.py
+++ urls/api/views.py
@@ -1,5 +1,5 @@
+from django.conf import settings
 from django.http import HttpResponseRedirect
-from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 
 from urls.models import Url
@@ -9,5 +9,11 @@ class RedirectAPIView(APIView):
     authorization_classes = []
 
     def get(self, *args, **kwargs):
-        url_obj = get_object_or_404(Url.objects.all_actives().only("url"), token=self.kwargs["token"])
-        return HttpResponseRedirect(redirect_to=url_obj.url)
+        url_obj = (Url.objects
+                   .all_actives()
+                   .filter(token=self.kwargs["token"])
+                   .only("url")
+                   .first())
+        if url_obj:
+            return HttpResponseRedirect(redirect_to=url_obj.url)
+        return HttpResponseRedirect(redirect_to=settings.URL_SHORTENER_404_PAGE)
