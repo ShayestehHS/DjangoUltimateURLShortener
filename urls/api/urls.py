883c10fe99f5c4dc312fa063fc0d04c1cd72e1ff
shayestehhs <shayestehhs1@gmail.com>
2024-06-17 21:14:42 +0330

    Update url name of redirect end-point

--- urls/api/urls.py
+++ urls/api/urls.py
@@ -4,5 +4,5 @@ from urls.api import views
 app_name = "urls"
 
 urlpatterns = [
-    path('<str:token>/', views.RedirectAPIView.as_view(), name='index'),
+    path('<str:token>/', views.RedirectAPIView.as_view(), name='redirect'),
 ]
