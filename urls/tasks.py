3b850e889ca0fddd704ed12d8d03c846bc2d149b
shayestehhs <shayestehhs1@gmail.com>
2024-06-17 22:22:20 +0330

    Add celery_beat task to create ready_to_set_token periodically

---
+++ urls/tasks.py
@@ -0,0 +1,14 @@
+from celery import shared_task
+from django.conf import settings
+
+from urls.models import Url
+
+
+@shared_task
+def create_ready_to_set_token():
+    ready_to_set_token_count = Url.objects.all_ready_to_set_token().count()
+    limit = settings.URL_SHORTENER_READY_TO_SET_TOKEN_LIMIT
+    if ready_to_set_token_count < limit:
+        for _ in range(limit - ready_to_set_token_count):
+            # ToDo: Use bulk_create
+            Url.objects.create_ready_to_set_token()
