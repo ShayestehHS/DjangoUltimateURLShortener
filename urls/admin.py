90518d6b61150723f5e3a409ce4e6498b58a57a4
shayestehhs <shayestehhs1@gmail.com>
2024-09-13 01:50:42 +0330

    Remove queryparam supports

--- urls/admin.py
+++ urls/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from django.db import models
 
 from urls.models import Url
-
-from flat_json_widget.widgets import FlatJsonWidget
 
 
 @admin.register(Url)
@@ -11,9 +8,6 @@ class UrlAdmin(admin.ModelAdmin):
     fields = ("url", "short_url", "created_at", "expiration_date")
     readonly_fields = ("short_url", "created_at",)
     list_display = ("__str__", "token", "created_at", "is_active")
-    formfield_overrides = {
-        models.JSONField: {'widget': FlatJsonWidget}
-    }
     ordering = ("-updated_at", )
     search_fields = ("token", "url")
 
