e1cc283948ecbf998d6ae512995799e3beff4ff4
shayestehhs <shayestehhs1@gmail.com>
2024-09-21 19:54:03 +0330

    Update URL admin.

--- urls/admin.py
+++ urls/admin.py
@@ -1,14 +1,38 @@
+from django.conf import settings
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
 
 from urls.models import Url
 
 
+class UrlAdminForm(ModelForm):
+    class Meta:
+        model = Url
+        fields = ("url", "token", "expiration_date")
+
+    def clean_token(self):
+        token = self.cleaned_data["token"]
+        if (Url.objects
+                .all_actives()
+                .exclude_ready_to_set_urls()
+                .filter(token=token)
+                .exists()):
+            raise ValidationError("This token is active.")
+
+    def clean_url(self):
+        url = self.cleaned_data["url"]
+        if url.startswith("http://"):
+            raise ValidationError("You can not use insecure URL.")
+        if url == settings.URL_SHORTENER_READY_TO_SET_TOKEN_URL:
+            raise ValidationError("You can not use this url because it is a reserved url.")
+
+
 @admin.register(Url)
 class UrlAdmin(admin.ModelAdmin):
-    fields = ("url", "short_url", "created_at", "expiration_date")
-    readonly_fields = ("short_url", "created_at",)
+    form = UrlAdminForm
     list_display = ("__str__", "token", "created_at", "is_active")
-    ordering = ("-updated_at", )
+    ordering = ("-updated_at",)
     search_fields = ("token", "url")
     search_help_text = "Search by 'URL' or 'Token' to quickly find specific records."
 
@@ -17,3 +41,11 @@ class UrlAdmin(admin.ModelAdmin):
 
     is_active.short_description = 'Is Active'
     is_active.boolean = True
+
+    def has_change_permission(self, request, obj=None):
+        if obj:
+            return False
+        return True
+
+    def save_model(self, request, obj, form, change):
+        Url.objects.create(**form.cleaned_data)
