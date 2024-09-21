d46ac9a5cb69669be31c219e36db9fcb73683ae7
shayestehhs <shayestehhs1@gmail.com>
2024-09-21 15:34:28 +0330

    Update the test names and minor code cleaning.

--- urls/tests/test_views.py
+++ urls/tests/test_views.py
@@ -22,23 +22,17 @@ class TestRedirectUrlView(APITestCase):
 
     @patch("urls.tasks.log_the_url_usages.delay")
     @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
-    def test_redirect_with_valid_token(self, mock_log_the_url_usages):
+    def test_redirect_view_with_valid_token_redirect_to_correct_url(self, mock_log_the_url_usages):
         url = Url.objects.create(url="https://example.com")
 
         with self.assertNumQueries(1):
             response = self.client.get(get_redirect_url(url.token))
             self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+            self.assertEqual(response["location"], url.url)
 
-        self.assertEqual(response["location"], url.url)
         mock_log_the_url_usages.assert_called_once()
 
-    def test_redirect_with_invalid_token(self):
-        with self.assertNumQueries(1):
-            response = self.client.get(get_redirect_url("abcdf"))
-            self.assertEqual(response.status_code, status.HTTP_302_FOUND)
-            self.assertEqual(response["location"], settings.URL_SHORTENER_404_PAGE)
-
-    def test_redirect_with_expired_token(self):
+    def test_redirect_view_with_expired_token_redirect_to_404_page(self):
         url = Url.objects.create(url="https://example.com", expiration_date=now() - timedelta(days=1))
 
         with self.assertNumQueries(1):
@@ -46,7 +40,7 @@ class TestRedirectUrlView(APITestCase):
             self.assertEqual(response.status_code, status.HTTP_302_FOUND)
             self.assertEqual(response["location"], settings.URL_SHORTENER_404_PAGE)
 
-    def test_redirect_with_ready_to_set_token(self):
+    def test_redirect_view_with_ready_to_set_token_redirect_to_404_page(self):
         url = Url.objects.create_ready_to_set_token()
 
         with self.assertNumQueries(1):
@@ -54,16 +48,27 @@ class TestRedirectUrlView(APITestCase):
             self.assertEqual(response.status_code, status.HTTP_302_FOUND)
             self.assertEqual(response["location"], settings.URL_SHORTENER_404_PAGE)
 
-    def test_redirect_with_long_token_raise_404(self):
-        with self.assertNumQueries(0):
-            token = "".join([choice(AVAILABLE_CHARS) for _ in range(settings.URL_SHORTENER_MAXIMUM_URL_CHARS + 1)])
-            url = get_redirect_url(token)
-            response = self.client.get(url)
-            self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, response.json())
+    def test_redirect_view_with_long_token_redirect_to_404_page(self):
+        token = "".join([choice(AVAILABLE_CHARS) for _ in range(settings.URL_SHORTENER_MAXIMUM_TOKEN_LENGTH + 1)])
 
-    def test_redirect_with_short_token_raise_404(self):
         with self.assertNumQueries(0):
-            token = "".join([choice(AVAILABLE_CHARS) for _ in range(settings.URL_SHORTENER_MAXIMUM_URL_CHARS - 1)])
+            response = self.client.get(get_redirect_url(token))
+            self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+            self.assertEqual(response["location"], settings.URL_SHORTENER_404_PAGE)
+
+    def test_redirect_view_with_invalid_token_redirect_to_404_page(self):
+        token = "".join([choice(AVAILABLE_CHARS) for _ in range(settings.URL_SHORTENER_MAXIMUM_TOKEN_LENGTH)])
+
+        with self.assertNumQueries(1):
+            response = self.client.get(get_redirect_url(token))
+            self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+            self.assertEqual(response["location"], settings.URL_SHORTENER_404_PAGE)
+
+    def test_redirect_view_with_short_token_redirect_to_404_page(self):
+        token = "".join([choice(AVAILABLE_CHARS) for _ in range(settings.URL_SHORTENER_MAXIMUM_TOKEN_LENGTH - 1)])
+
+        with self.assertNumQueries(0):
             url = get_redirect_url(token)
             response = self.client.get(url)
-            self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, response.json())
+            self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+            self.assertEqual(response["location"], settings.URL_SHORTENER_404_PAGE)
