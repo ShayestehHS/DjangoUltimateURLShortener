53c779bd73ac145cbe22553ad752816e11a23f47
shayestehhs <shayestehhs1@gmail.com>
2024-10-04 20:35:47 +0330

    Fix token max_length

--- urls/models.py
+++ urls/models.py
@@ -27,7 +27,7 @@ def get_default_expiration_date():
 
 class Url(TimeStampModel):
     url = models.URLField(max_length=255, validators=[is_https])
-    token = models.CharField(max_length=MAXIMUM_URL_LENGTH)
+    token = models.CharField(max_length=MAXIMUM_TOKEN_LENGTH)
     expiration_date = models.DateTimeField(default=get_default_expiration_date)
 
     objects = UrlManager()
